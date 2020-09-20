98ac926f45c8c4e292ff43ae6ca5dc25b62fd7cd
didie <dedtom@gmail.com>
2020-09-21 01:06:05 +0200

    *

--- deploy/yq_inline.sh
+++ deploy/yq_inline.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-yq write -i argo_repo/deploy/deployment.yaml 'spec.template.spec.containers.image' $(yq read image_tags.yaml image)
+yq write -i argo_repo/deploy/deployment.yaml 'spec.template.spec.containers.*.image' $(yq read image_tags.yaml image)
 
