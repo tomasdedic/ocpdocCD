6baef3f4ce106560472976d5f7cad9c330c22e5b
didie <dedtom@gmail.com>
2021-02-26 19:36:40 +0100

    aks

--- yq_inline.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#yq write -i argo_repo/deploy/deployment.yaml 'spec.template.spec.containers.*.image' $(yq read image_tags.yaml image)
-yq write -i argo_repo/deploy/deployment.yaml 'spec.template.spec.containers.[0].image' $(yq read argo_repo/image_tags.yaml image)
-yq write -i argo_repo/deploy_proxy/apps_v1_deployment_proxy.yaml 'spec.template.spec.containers.[1].image' $(yq read argo_repo/image_tags.yaml image)
-
